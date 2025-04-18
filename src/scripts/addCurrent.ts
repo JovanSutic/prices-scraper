8353d556794f7c68935d0355a2e73a5d7940db4b
unknown <josutic.sutic@gmail.com>
2025-04-18 21:32:42 +0200

    feat(5): remove token

--- src/scripts/addCurrent.ts
+++ src/scripts/addCurrent.ts
@@ -5,9 +5,8 @@ import { prepCurrent, prepPrices } from "../utils/prep";
 import { scrapeCurrent } from "../utils/scrapCurrent";
 
 (async function () {
-  const token =
-    "eyJraWQiOiJDemVYd3FTRzg1ZDhDRmFOTWI5XC9aZFZWa0hGeEdWTTJ2Qk5xSFVLT1RTWT0iLCJhbGciOiJSUzI1NiJ9.eyJzdWIiOiI1YXJqN2c2MDdtNXVuc2o0ZWJsdnBtNGxrMyIsInRva2VuX3VzZSI6ImFjY2VzcyIsInNjb3BlIjoibmVzdEFwaVJTXC93cml0ZSIsImF1dGhfdGltZSI6MTc0NTAwMzMwMywiaXNzIjoiaHR0cHM6XC9cL2NvZ25pdG8taWRwLmV1LWNlbnRyYWwtMS5hbWF6b25hd3MuY29tXC9ldS1jZW50cmFsLTFfeWZvYXZYU1BuIiwiZXhwIjoxNzQ1MDA2OTAzLCJpYXQiOjE3NDUwMDMzMDMsInZlcnNpb24iOjIsImp0aSI6IjY3MDRiNTQ5LWRkMWEtNGIwNC05ZjJlLTY3MWU1NTdlZDlhZCIsImNsaWVudF9pZCI6IjVhcmo3ZzYwN201dW5zajRlYmx2cG00bGszIn0.TVJy0iiehzS8D0rUxGhZSo2_NNSHwF_PxZyASS9Evu1MA1CBOTVVOAl2dO7dzlbgqC1OX2OIjJCDcIzPseQXpg_R83vO0BvnTozCzuB7tLbd8KFpHNMTTMhKsaWKE3gIBDRTxwu6AyOzGhQdaPssY3YdAahFBuqnhjoYbE98A_Ry24pDmrc1c911177DmoUz7hLQniWsbVQdnT2qfVcwzF4a3-f8O0uvYCx88jTWY0lqEYKrCZRehfMjxBQZJF9yPBVQcMdXR4CMwdm2EeaaKb57ypGtjJx0ZJTgvw_rNxXG2YvtTUOOBzd5RP79n-tMEPsR4urx0i7yIEOyTtdNVA";
-  const baseUrl = "http://3.77.158.72:3000/";
+  const token = process.env.AUTH_TOKEN;
+  const baseUrl = process.env.BASE_URL;
   const scrapUrl =
     "https://www.numbeo.com/cost-of-living/in/Belgrade?displayCurrency=EUR";
 
@@ -91,7 +90,6 @@ import { scrapeCurrent } from "../utils/scrapCurrent";
       city: 1,
       categories: categoriesMap,
     });
-    // console.log(prices);
 
     if (prices.length) {
       console.log("SAVING PRICES UNDER WAY");
