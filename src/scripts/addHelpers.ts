c2255914948c47542cc550ce4f78749759bc4024
unknown <josutic.sutic@gmail.com>
2025-05-18 17:26:06 +0200

    feat(10): getting updates for new cities

--- src/scripts/addHelpers.ts
+++ src/scripts/addHelpers.ts
@@ -1,12 +1,24 @@
 import { ProductType } from "../types/api";
-import { addProductType } from "../utils/scrapCurrent";
+import { SocialType } from "../types/utils";
+import { addBudgetType, addProductType } from "../utils/scrapCurrent";
 
 (async function () {
   const token = process.env.AUTH_TOKEN;
 
   if (token) {
     try {
+      console.log("addProductType start");
       await addProductType(ProductType.ALL, token);
+    } catch (error) {
+      if (error instanceof Error) {
+        console.log(error.message);
+        throw error;
+      }
+    }
+
+    try {
+      console.log("addBudgetType start");
+      await addBudgetType(SocialType.SOLO, token);
     } catch (error) {
       if (error instanceof Error) {
         console.log(error.message);
